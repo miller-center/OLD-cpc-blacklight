47e6537cf9e2f4fe32af6a96ee50dc31f7d9c67a
Ben Taitelbaum <ben@coshx.com>
2012-11-27 23:20:42 -0500

    Revert "adds routes to handle IDs that include periods"
    This reverts commit df2b5b8ca84c87d576341b0183625db978938389.

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,6 @@ Imls::Application.routes.draw do
 
   Blacklight.add_routes(self)
 
-  # override some blacklight routes
-  # to handle IDs that include periods
-  get '/catalog/:*id', :format => false, :as => 'solr_document', :to => 'catalog#show'
-  get '/catalog/:*id', :format => false, :as => 'catalog', :to => 'catalog#show'
-  put '/catalog/:*id', :format => false, :as => 'solr_document', :to => 'catalog#update'
-  put '/catalog/:*id', :format => false, :as => 'catalog', :to => 'catalog#update'
-
-  match '/catalog/:*id/librarian_view(.:format)', :as => 'librarian_view_catalog', :to => 'catalog#librarian_view'
   devise_for :users
 
 end
